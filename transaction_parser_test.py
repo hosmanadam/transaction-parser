54888b4703c0414a20a6be433a72b980c24e7acf
Adam Hosman <github@adamsinbox.com>
2019-03-28 15:33:20 +0100

    Draft additional test cases with fixtures

--- transaction_parser_test.py
+++ transaction_parser_test.py
@@ -43,14 +43,38 @@ fixtures = [
 
     {
         'inputs': [
-            '1400+(2*400)huf istanbul kebab eating out  // transaction comment  # metacomment',
+            '900HUF ALDI Groceries',                      # processes_transaction_in_varied_case
+            '900huf aldi groceries',                      # processes_transaction_in_lower_case
+            '   900huf   aldi      groceries',            # ignores_extra_inline_whitespace
+            '900 ALDI Groceries',                         # adds_default_currency
+            '900HUF ALDI',                                # adds_default_category
+            '900HUF ALDI food',                           # figures_out_subcategory_from_category
+            '300 + 600 ALDI Groceries',                   # handles_explicit_math
+            '300 600 ALDI Groceries',                     # handles_implicit_math
+        ],
+        'expected': [
+            {
+                'amount_hundredths': 90000,
+                'currency_code': 'huf',
+                'partner': 'aldi',
+                'category': 'Groceries',
+                'transaction_comment': None,
+                'metacomment': None
+            },
+        ]
+    },
+
+
+    {
+        'inputs': [
+            '1400+(2*400)huf istanbul kebab eating out  // transaction comment  # metacomment',  # handles_body_without_exceptions
         ],
         'expected': [
             {
                 'amount_hundredths': 220000,
                 'currency_code': 'huf',
                 'partner': 'istanbul kebab',
-                'category': 'eating out',
+                'category': 'Eating out',
                 'transaction_comment': 'transaction comment',
                 'metacomment': 'metacomment'
             },
@@ -59,14 +83,14 @@ fixtures = [
 
     {
         'inputs': [
-            '14000-5000huf tgi fridays eating out of 3*1000 drinking out 1500 tip  // used amex, Amy chipped in',
+            '14000-5000huf tgi fridays eating out of 3*1000 drinking out 1500 tip  // used amex, Amy chipped in',  # handles_body_with_exceptions
         ],
         'expected': [
             {
                 'amount_hundredths': 450000,
                 'currency_code': 'huf',
                 'partner': 'tgi fridays',
-                'category': 'eating out',
+                'category': 'Eating out',
                 'transaction_comment': 'used amex, Amy chipped in',
                 'metacomment': None
             },
@@ -74,7 +98,7 @@ fixtures = [
                 'amount_hundredths': 300000,
                 'currency_code': 'huf',
                 'partner': 'tgi fridays',
-                'category': 'drinking out',
+                'category': 'Drinking out',
                 'transaction_comment': 'used amex, Amy chipped in',
                 'metacomment': None
             },
@@ -82,7 +106,7 @@ fixtures = [
                 'amount_hundredths': 150000,
                 'currency_code': 'huf',
                 'partner': 'tgi fridays',
-                'category': 'tip',
+                'category': 'Tip',
                 'transaction_comment': 'used amex, Amy chipped in',
                 'metacomment': None
             },
@@ -98,13 +122,37 @@ class TestParseTransactionBody:
     def run_it(fixture_index, input_index):
         parsed_transaction = parse_transaction_body(
             fixtures[fixture_index]['inputs'][input_index],
-            CATEGORY_SHORTHANDS,
-            config.ALL_CURRENCY_CODES
+            SHORTHANDS_TO_CATEGORIES,
+            config.ALL_CURRENCY_CODES,
         )
         assert parsed_transaction == fixtures[fixture_index]['expected']
 
+    # def test_processes_transaction_in_varied_case(self):
+    #     self.run_it(0, 0)
+    #
+    # def test_processes_transaction_in_lower_case(self):
+    #     self.run_it(0, 1)
+    #
+    # def test_ignores_extra_inline_whitespace(self):
+    #     self.run_it(0, 2)
+    #
+    # def test_adds_default_currency(self):
+    #     self.run_it(0, 3)
+    #
+    # def test_adds_default_category(self):
+    #     self.run_it(0, 4)
+    #
+    # def test_figures_out_subcategory_from_category(self):
+    #     self.run_it(0, 5)
+    #
+    # def test_handles_explicit_math(self):
+    #     self.run_it(0, 6)
+    #
+    # def test_handles_implicit_math(self):
+    #     self.run_it(0, 7)
+
     def test_handles_body_without_exceptions(self):
-        self.run_it(0, 0)
+        self.run_it(1, 0)
 
     def test_handles_body_with_exceptions(self):
-        self.run_it(1, 0)
+        self.run_it(2, 0)
